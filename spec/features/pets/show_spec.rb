7332dd4a2448e3fd2428f84c4589bb18c80afafb
mkbonini <mkbonini@gmail.com>
2022-07-19 13:30:52 -0600

    Tests: application approval makes pets unadoptable

--- spec/features/pets/show_spec.rb
+++ spec/features/pets/show_spec.rb
@@ -25,4 +25,32 @@ RSpec.describe 'the shelter show' do
     expect(page).to have_current_path('/pets')
     expect(page).to_not have_content(pet.name)
   end
+
+  it 'approved applications makes pets unadoptable' do
+    application = Application.create!(name: 'John Doe', street_address: '123 apple street', city: 'Denver', state: 'CO', zipcode: '90210', description: 'we love pets', status: 'In Progress')
+    shelter = Shelter.create!(name: 'Mystery Building', city: 'Irvine CA', foster_program: false, rank: 9)
+    scooby = Pet.create!(name: 'Scooby', age: 2, breed: 'Great Dane', adoptable: true, shelter_id: shelter.id)
+    clifford = Pet.create!(name: 'Clifford', age: 1, breed: 'Red Dog', adoptable: true, shelter_id: shelter.id)
+
+    PetApplication.create!(pet: scooby, application: application)
+    PetApplication.create!(pet: clifford, application: application)
+
+    visit "/admin/applications/#{application.id}"
+
+    within "#pets-#{scooby.id}" do 
+      click_on("Approve")
+    end  
+
+    within "#pets-#{clifford.id}" do 
+      click_on("Approve")
+    end 
+    
+    visit "/pets/#{scooby.id}"
+    expect(page).to have_content("false")
+    expect(page).to_not have_content("true")
+
+    visit "/pets/#{clifford.id}"
+    expect(page).to have_content("false")
+    expect(page).to_not have_content("true")
+  end
 end
