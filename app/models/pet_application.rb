c118419f4e69a87e6f1bf0e0a409fc24a5f27c89
mkbonini <mkbonini@gmail.com>
2022-07-19 13:01:00 -0600

    Accepts application when all pets are reviewed

--- app/models/pet_application.rb
+++ app/models/pet_application.rb
@@ -1,4 +1,12 @@
 class PetApplication < ApplicationRecord
   belongs_to :pet
   belongs_to :application
+
+  # def completed? 
+  #   if select(:pet_status).include?(nill)
+  #     return false
+  #   else 
+  #     return true
+  #   end
+  # end
 end
